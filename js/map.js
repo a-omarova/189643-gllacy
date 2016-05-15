f8a909769f0e5dec9c0e5c3558c803d3a32e52fb
a-omarova <mamikami@yandex.ru>
2016-05-15 21:16:07 +0300

    стили

---
+++ js/map.js
@@ -0,0 +1,25 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.93863075783236,30.323054499999976], 
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'pin2.png',
+            // Размеры метки.
+            iconImageSize: [218, 142],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [10, 42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
